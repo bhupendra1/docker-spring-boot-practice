409984eb3341c0e8a69ecb22aee3f1d47155284a
Bhupendra Karki <bhupendrakarki@bhupendras-mbp.home>
2021-02-17 09:57:50 -0600

    Practice creating docker image from spring boot app and packaging it inside the container and deploying to docker

---
+++ src/test/java/com/bhupendra/docker/dockerspringboot/DockerSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhupendra.docker.dockerspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
